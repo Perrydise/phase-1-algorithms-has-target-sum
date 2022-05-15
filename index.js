4867b2f901ebd5a65a33af40ac0eab9b3a7a2737
Perry Jorss <pjorss123@gmail.com>
2022-05-15 17:07:39 -0400

    commit tree 6e305951c7cfdcabd140074c654ffc66887e3e93

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true
+    }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -9,6 +17,8 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
+//add all the numbers in the array
+//if the numbers = the target then return true
 
 /*
   Add written explanation of your solution here
